eb1bddc7f5b6c9afbdfd62570f6d97f099b90a44
Luke Rocco <luke.rocco@globalgaming.com>
2017-11-17 12:25:04 +0100

    added components index file for easier imports

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+// Atoms
+import NavLink from './atoms/nav-link'
+
+// Pages
+import Home from './pages/home'
+import About from './pages/about'
+import FourOhFour from './pages/error'
+
+// EXPORTS
+export {
+  NavLink,
+  Home,
+  About,
+  FourOhFour
+}
