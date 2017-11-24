8408390bfa0d535a433c8c525145b2878f260715
Luke Rocco <luke.rocco@globalgaming.com>
2017-11-24 15:02:17 +0100

    updated storybook config: replaced stories with story

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { configure } from '@storybook/react'
 
-const req = require.context('../../src', true, /.stories.js$/)
+const req = require.context('../../src', true, /.story.js$/)
 
 function loadStories() {
   req.keys().map(story => req(story))
