0453a7ac688c006230e7d2c1c47a38d6d09ea547
Luke Rocco <luke.rocco@globalgaming.com>
2017-11-24 16:07:58 +0100

    reusing Button atom in Home page

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button } from 'atom-c'
+import { Button } from '~components'
 
 export default class Home extends Component {
   constructor ({ props }) {
@@ -10,19 +10,19 @@ export default class Home extends Component {
     return (
       <article className="page-home">
         <h1>Home</h1>
-        <p>Count: {this.props.count}</p>
+        <p>Count: { this.props.count }</p>
 
         <p>
-          <Button onClick={this.props.increment} disabled={this.props.isIncrementing}>Increment</Button>
-          <Button onClick={this.props.incrementAsync} disabled={this.props.isIncrementing}>Increment Async</Button>
+          <Button onClick={ this.props.increment } disabled={ this.props.isIncrementing }>Increment</Button>
+          <Button onClick={ this.props.incrementAsync } disabled={ this.props.isIncrementing }>Increment Async</Button>
         </p>
 
         <p>
-          <Button onClick={this.props.decrement} disabled={this.props.isDecrementing}>Decrementing</Button>
-          <Button onClick={this.props.decrementAsync} disabled={this.props.isDecrementing}>Decrement Async</Button>
+          <Button onClick={ this.props.decrement } disabled={ this.props.isDecrementing }>Decrementing</Button>
+          <Button onClick={ this.props.decrementAsync } disabled={ this.props.isDecrementing }>Decrement Async</Button>
         </p>
 
-        <p><Button onClick={() => this.props.changePage()}>Go to about page via redux</Button></p>
+        <p><Button onClick={ () => this.props.changePage() }>Go to about page via redux</Button></p>
       </article>
     )
   }
