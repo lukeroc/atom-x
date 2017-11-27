e5aca94f52e2ad1ce0f40eb3f6b1497f78e27170
Luke Rocco <luke.rocco@globalgaming.com>
2017-11-27 09:06:57 +0100

    updated home class

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -2,27 +2,35 @@ import React, { Component } from 'react'
 import { Button } from '~components'
 
 export default class Home extends Component {
-  constructor ({ props }) {
-    super(props)
-  }
-
   render () {
+
+    const {
+      changePage,
+      count,
+      decrement,
+      decrementAsync,
+      increment,
+      incrementAsync,
+      isDecrementing,
+      isIncrementing
+    } = this.props
+
     return (
       <article className="page-home">
         <h1>Home</h1>
-        <p>Count: { this.props.count }</p>
+        <p>Count: { count }</p>
 
         <p>
-          <Button onClick={ this.props.increment } disabled={ this.props.isIncrementing }>Increment</Button>
-          <Button onClick={ this.props.incrementAsync } disabled={ this.props.isIncrementing }>Increment Async</Button>
+          <Button onClick={ increment } disabled={ isIncrementing }>Increment</Button>
+          <Button onClick={ incrementAsync } disabled={ isIncrementing }>Increment Async</Button>
         </p>
 
         <p>
-          <Button onClick={ this.props.decrement } disabled={ this.props.isDecrementing }>Decrementing</Button>
-          <Button onClick={ this.props.decrementAsync } disabled={ this.props.isDecrementing }>Decrement Async</Button>
+          <Button onClick={ decrement } disabled={ isDecrementing }>Decrementing</Button>
+          <Button onClick={ decrementAsync } disabled={ isDecrementing }>Decrement Async</Button>
         </p>
 
-        <p><Button onClick={ () => this.props.changePage() }>Go to about page via redux</Button></p>
+        <p><Button onClick={ () => changePage() }>Go to error page via redux</Button></p>
       </article>
     )
   }
