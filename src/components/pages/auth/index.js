b08d3a86f274e35cd0e7d287bbd050dd24bf789c
Luke Rocco <luke.rocco@globalgaming.com>
2017-11-30 16:24:07 +0100

    removed unused import from auth page page

--- src/components/pages/auth/index.js
+++ src/components/pages/auth/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
-import AuthForm from '~organisms/auth-form'
 import AuthLayout from '~templates/auth'
+import AuthForm from '~containers/auth-form'
 
 export default class Auth extends Component {
   render () {
