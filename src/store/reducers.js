9794e2a2ee8b7bf992c18d120d28428c5f10f4b6
Luke Rocco <luke.rocco@globalgaming.com>
2017-11-30 14:26:21 +0100

    added redux-form reducer in combineReducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
+import { reducer as formReducer } from 'redux-form'
+
 import counter from './counter/reducer'
+import authentication from './authentication/reducer'
 
 export default combineReducers({
   routing: routerReducer,
-  counter
+  counter,
+  authentication,
+  form: formReducer
 })
